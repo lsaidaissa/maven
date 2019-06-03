7dad152a1ce10a3d714edfc4672f4e15e6a38ba8
Said Aissa <lyas.saidaissa@ts-consult.com>
2019-06-04 01:24:40 +0200

    changing compile plugin to 1.8

--- src/main/java/com/lyas/training/Application.java
+++ src/main/java/com/lyas/training/Application.java
@@ -1,6 +1,19 @@
 package com.lyas.training;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Application {
+	
+	public void greet() {
+		List<String> greetings = new ArrayList<>();
+		greetings.add("Hello");
+		
+		for (String greeting: greetings) {
+			System.out.println("greeting : "+greeting);
+		}
+		
+	}
 	
 	public Application() {
 		System.out.println("inside application");
@@ -9,6 +22,7 @@ public class Application {
 	public static void main(String[] args) {
 		System.out.println("starting application");
 		Application application = new Application();
+		application.greet();
 	}
 	
 }
