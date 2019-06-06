504fa31a6051798e38471a23127ff8d4f1fd4464
Said Aissa <lyas.saidaissa@ts-consult.com>
2019-06-06 23:00:46 +0200

    add dependency to maven

--- src/main/java/com/lyas/training/Application.java
+++ src/main/java/com/lyas/training/Application.java
@@ -2,8 +2,13 @@ package com.lyas.training;
 
 import java.util.List;
 import java.util.ArrayList;
+import org.apache.commons.lang3.StringUtils;
 
 public class Application {
+	
+	public int countWords(String words) {
+		return (words == null || words.isEmpty()) ? 0 : StringUtils.split(words, " ").length;
+	}
 	
 	public void greet() {
 		List<String> greetings = new ArrayList<>();
@@ -23,6 +28,8 @@ public class Application {
 		System.out.println("starting application");
 		Application application = new Application();
 		application.greet();
+		int count = application.countWords("I have four words");
+		System.out.println("count : "+count);
 	}
 	
 }
